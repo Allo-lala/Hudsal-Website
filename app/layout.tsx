cd623992225d7b6736840190de01015ad8361743
Allan <allankyagulanyi8@gmail.com>
2026-02-09 20:50:53 +0300

    suppress the hydration issue in the layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -125,7 +125,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} font-sans antialiased`}>
+      <body className={`${poppins.variable} font-sans antialiased`} suppressHydrationWarning>
         <LoadingProvider>
           {children}
         </LoadingProvider>
